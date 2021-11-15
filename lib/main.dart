072922acb8cf7a0f0e6e1411a65616b9b3f981e9
Hugo Rodríguez <hugsrodriguez8@gmail.com>
2021-11-14 21:15:27 -0600

    Preestableciendo el modo claro de la App

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.light,
       debugShowCheckedModeBanner: false,
       title: 'ArtMus',
       initialRoute: '/',
